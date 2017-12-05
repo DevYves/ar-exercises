60bf172026c75d04e4abfb605fc4dd40e06496fb
DevYves <yfreypons@gmail.com>
2017-12-05 20:48:14 +0000

    Completed exercise 2, updating store 1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+
+### Exercise 2: Update the first store
+
+@store1 =Store.find(1)
+@store2 =Store.find(2)
+@store1.name = "Yves' Coats"
+@store1.save
+

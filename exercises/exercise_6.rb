fec3f48a170620aa16de36b8e9e6af21e09747f1
DevYves <yfreypons@gmail.com>
2017-12-05 22:34:24 +0000

    modified employee and store table and added employees to database

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+
+
+ @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+ @store1.employees.create(first_name: "Daniel", last_name: "Hanford", hourly_rate: 30)
+ @store1.employees.create(first_name: "Melissa", last_name: "Levinson", hourly_rate: 50)
+ @store2.employees.create(first_name: "Han", last_name: "Solo", hourly_rate: 200)
+ @store2.employees.create(first_name: "Mel", last_name: "On", hourly_rate: 10)
+ @store2.employees.create(first_name: "Ken", last_name: "Nicholson", hourly_rate: 25)
+ @store2.employees.create(first_name: "Manfred", last_name: "Regatano", hourly_rate: 100)
+ @store2.employees.create(first_name: "Archie", last_name: "Andrews", hourly_rate: 20)
